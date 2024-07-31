66a3ffb063d4bbeb7bb6d3b36bf67a3fe209af62
Huong10102004 <150785660+Huong10102004@users.noreply.github.com>
2024-07-31 23:27:36 +0700

    base project

---
+++ src/layouts/PrimaryLayout/PrimaryLayout.js
@@ -0,0 +1,15 @@
+import Footer from "../../Components/Footer";
+import Header from "../../Components/Header";
+import Sidebar from "../../Components/Sidebar";
+
+function PrimaryLayout() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </>
+  );
+}
+
+export default PrimaryLayout;
